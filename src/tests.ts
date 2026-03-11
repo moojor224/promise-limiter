94606911da5d9e2da3b74569f458afb2dae40884
moojor224 <moojor224@gmail.com>
2026-03-11 15:41:26 -0500

    Update tests.ts

--- src/tests.ts
+++ src/tests.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import pLimit, { type LimitFunction } from "p-limit";
 import { ConcurrencyLimiter } from "./concur";
 import type { Limiter } from "./types";
@@ -10,10 +11,14 @@ function TS(id: number) {
         });
     };
 }
+
+const taskConcur = 5; // run X tasks at once
+const taskCount = 20; // this many times
+
 async function testPLimit(limit: LimitFunction, expectedLength: number) {
     const start = Date.now();
     const promises: Promise<any>[] = [];
-    for (let i = 0; i < 99; i++) {
+    for (let i = 0; i < taskCount * taskConcur; i++) {
         promises.push(limit(TS(i)));
     }
     await Promise.all(promises);
@@ -25,7 +30,7 @@ async function testPLimit(limit: LimitFunction, expectedLength: number) {
 
 async function test(limit: Limiter, expectedLength: number) {
     const start = Date.now();
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < taskCount * taskConcur + 1; i++) {
         limit.run(TS(i));
     }
     await limit.wait();
@@ -39,10 +44,12 @@ async function test(limit: Limiter, expectedLength: number) {
 async function run() {
     const as: number[] = [];
     const bs: number[] = [];
-    for (let i = 0; i < 1; i++) {
-        // await test(new RateLimiter(2), 4500);
-        let a = await Promise.all(new Array(100).fill(0).map((e) => test(new ConcurrencyLimiter(3), 3000)));
-        let b = await Promise.all(new Array(100).fill(0).map((e) => testPLimit(pLimit(3), 3000)));
+    for (let i = 0; i < 10; i++) {
+        // run 10 rounds of tests
+        // run 5 limiters at the same time
+        let a = await Promise.all(new Array(5).fill(0).map((e) => test(new ConcurrencyLimiter(taskConcur), 3000)));
+        let b = await Promise.all(new Array(5).fill(0).map((e) => testPLimit(pLimit(taskConcur), 3000)));
+
         // await test(new ConcurrencyRateLimiter(2, 1), 9000);
         let aavg = a.reduce((a, b) => a + b, 0) / a.length;
         let bavg = b.reduce((a, b) => a + b, 0) / b.length;
@@ -50,8 +57,11 @@ async function run() {
         as.push(aavg);
         bs.push(bavg);
     }
-    console.log((as.reduce((a, b) => a + b, 0) / as.length).toFixed(2));
-    console.log((bs.reduce((a, b) => a + b, 0) / bs.length).toFixed(2));
-    throw new Error();
+    const tas = as.reduce((a, b) => a + b, 0) / as.length;
+    const tbs = bs.reduce((a, b) => a + b, 0) / bs.length;
+    console.log(tas.toFixed(2));
+    console.log(tbs.toFixed(2));
+    console.log((tas - tbs).toFixed(2));
+    process.exit(0);
 }
 run();
